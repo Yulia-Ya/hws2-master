38a51d1ddffc90e729f4b0e8954203c2314fed97
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-07-11 13:30:51 +0300

    hw5-2

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -19,11 +19,11 @@ function Pages() {
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 
-                <Route path="/" element={<Navigate to={'PRE_JUNIOR'}/>}/> 
+                <Route path="/" element={<Navigate to={PATH.PRE_JUNIOR}/>}/> 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                <Route path='PRE_JUNIOR' element={<PreJunior/>}/>
-                <Route path='JUNIOR'element={<Junior/>}/>
-                <Route path='JUNIOR_PLUS'element={<JuniorPlus/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
             
                 <Route path='/*' element={<Error404/>}/> 
